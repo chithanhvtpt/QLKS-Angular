866ed1b88837219044dc817df5fbf393c32c0df1
Nguyễn Mạnh Chí Thành <chithanhvtpt@gmail.com>
2022-01-02 18:59:46 +0700

    register done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/pages/login/login.component";
 import {RegisterComponent} from "./components/pages/register/register.component";
+import {MasterComponent} from "./components/layouts/master/master.component";
 
 const routes: Routes = [
+  {
+    path: "", component: MasterComponent
+  },
+
   {
     path: "login", component: LoginComponent
   },
