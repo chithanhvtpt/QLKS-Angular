b92578a737c19496695f116ec9798068dc9e26ce
Nguyễn Mạnh Chí Thành <chithanhvtpt@gmail.com>
2022-01-03 11:35:06 +0700

    login done

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {LoginService} from "../../../services/auth/login.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup | any
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router, private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ["", Validators.required, Validators.email],
+      password: ["", Validators.required]
+    })
+  }
+  login() {
+    let data = this.loginForm.value
+    this.loginService.login(data).subscribe(res => {
+      this.router.navigate([""])
+    })
   }
 
 }
