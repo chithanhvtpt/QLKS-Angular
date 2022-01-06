9c21259702ff9dc21d25bd4be70eb1c8d482c5c8
Nguyễn Mạnh Chí Thành <chithanhvtpt@gmail.com>
2022-01-06 09:08:36 +0700

    register success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,8 @@ import { FooterComponent } from './components/layouts/footer/footer.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
 import { HouseListComponent } from './components/houses/house-list/house-list.component';
-import { UserComponent } from './components/user/user.component';
-import { UserListComponent } from './users/user-list/user-list.component';
-import { UserUpdateComponent } from './users/user-update/user-update.component';
 import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import {UserUpdateComponent} from "./components/users/user-update/user-update.component";
 
 
 @NgModule({
@@ -28,8 +26,6 @@ import { UserDetailComponent } from './components/users/user-detail/user-detail.
     HeaderComponent,
     FooterComponent,
     HouseListComponent,
-    UserComponent,
-    UserListComponent,
     UserUpdateComponent,
     UserDetailComponent
   ],
